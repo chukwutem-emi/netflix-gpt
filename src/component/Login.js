bc517ab39c72c1401af3efabc2940564a4ea9077
chukwutem-emi <chukwutememi@gmail.com>
2024-09-22 22:12:28 +0100

    updated

--- src/component/Login.js
+++ src/component/Login.js
@@ -14,9 +14,9 @@ const Login = () => {
         </div>
         <form className='w-[30rem] my-40 mx-auto shadow-lg p-8 bg-black bg-opacity-80 absolute z-40'>
             <h1 className='text-3xl font-bold text-white mb-[1.5rem]'>{isSignInForm ? "Sign In" : "Sign Up"}</h1>
-            {!isSignInForm && (<input type= "text"  placeholder="Full Name" className="p-4 outline-none w-[100%] bg-gray-700 rounded-lg mb-[1.5rem]" autoComplete='on' autoFocus/>)}
-            <input type= "email"  placeholder="email address" className="p-4 outline-none w-[100%] bg-gray-700 rounded-lg mb-[1.5rem]" autoComplete='on' autoFocus/>
-            <input type= "password"  placeholder="password" className="p-4 outline-none w-[100%] bg-gray-700 rounded-lg mb-[1.5rem]"/>
+            {!isSignInForm && (<input type= "text"  placeholder="Full Name" className="p-4 w-[100%] bg-gray-700 rounded-lg mb-[1.5rem] text-white" autoComplete='on' autoFocus required/>)}
+            <input type= "email"  placeholder="email address" className="p-4 w-[100%] bg-gray-700 rounded-lg mb-[1.5rem] text-white" autoComplete='on' autoFocus required/>
+            <input type= "password"  placeholder="password" className="p-4 w-[100%] bg-gray-700 rounded-lg mb-[1.5rem] text-white" autoFocus required/>
             <button type="submit" className=" p-4 w-[100%] bg-red-400 rounded-lg text-white font-sans mb-[1.5rem]" title='click to sign in'>{isSignInForm ? "Sign In" : "Sign Up"}</button>
             <p className='text-white ml-4 cursor-pointer' onClick={toggleSignInForm}>{isSignInForm ? "New to Netflix? Sign Up Now" : "Already registered?, Sign In Now"}</p>
         </form>
